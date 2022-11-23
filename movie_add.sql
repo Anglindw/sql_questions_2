ebef45e0b3ee5804332f1b3043323f0ec6ec2fdf
Anglindw <dwanglin@gmail.com>
2022-11-23 14:07:41 -0600

    final

---
+++ movie_add.sql
@@ -0,0 +1,52 @@
+INSERT INTO customer(
+    first_name,
+    last_name
+)
+
+VALUES(
+    'Duncan',
+    'Anglin'
+),
+(
+    'Jackson',
+    'Polluck'
+);
+
+INSERT INTO concessions(
+    product_name,
+    product_type,
+    price
+)
+
+VALUES(
+    'Twizzlers',
+    'candy',
+    5
+),
+(
+    'Franks',
+    'main',
+    '10'
+),
+
+(
+    'Icee',
+    'drinks',
+    '8'
+);
+
+INSERT INTO movie(
+    title,
+    ticket_price,
+    runtime
+)
+VALUES(
+    'Jurrasic Park',
+    '15',
+    '3 hours 5 minutes'
+),
+(
+    'Top Gun: Maverick',
+    '10',
+    '2 hours 45 minutes'
+);
